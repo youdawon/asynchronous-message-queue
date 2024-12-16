b0323ab17ae28c10c5513efec4965e3bad43e537
youdawon <ydawon0514@gmail.com>
2024-12-16 14:01:07 +0900

    test script edited

--- test/test_message_subscriber.py
+++ test/test_message_subscriber.py
@@ -1,24 +1,23 @@
 import pytest
 from src.service_b.message_subscriber import MessageSubscriber
-
+from src.utils.config import ALLOWED_TYPE
 
 """
 MESSAGE_FILTER_MODE defines the message filtering behavior.
 
 - "allow_all": Allows all messages to be processed, regardless of their type.
-- "specific_type": Only processes messages where the 'type' parameter matches "serviceB".
+- "specific_type": Only processes messages where the 'type' parameter matches ALLOWED_TYPE.
 
 Usage:
 To filter messages by type, set the mode to "specific_type". 
-In this case, only messages with a 'type' of "serviceB" will be forwarded. If you want to allow all messages, set the mode to "allow_all".
+In this case, only messages with a 'type' matching ALLOWED_TYPE will be forwarded. If you want to allow all messages, set the mode to "allow_all".
 """
 @pytest.mark.parametrize(
     "message, filter_mode, expected",
     [
-        ({"type": "serviceA", "content": "test"}, "allow_all", True),
-        ({"type": "serviceB", "content": "test"}, "specific_type", True),
-        ({"type": "serviceA", "content": "test"}, "specific_type", False),
-        ({"type": "unknown", "content": "test"}, "unknown_filter", False),
+        ({"type": "type_test", "content": "test"}, "allow_all", True),  
+        ({"type": ALLOWED_TYPE, "content": "test"}, "specific_type", True),  
+        ({"type": "different_type", "content": "test"}, "specific_type", False), 
     ],
 )
 def test_is_allowed_message_type(message, filter_mode, expected):
